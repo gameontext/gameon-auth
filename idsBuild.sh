bd86ba3c6e3aa321e53f66113b8a97c847ec87eb
Erin Schnabel <ebullient@users.noreply.github.com>
2016-06-21 08:59:55 -0400

    debug shell
    Signed-off-by: Erin Schnabel <ebullient@users.noreply.github.com>

--- idsBuild.sh
+++ idsBuild.sh
@@ -6,14 +6,7 @@
 
 #!/bin/bash
 echo Informing Slack...
-curl -X 'POST' --silent --data-binary '{"text":"A new build for the player service has started."}' $SLACK_WEBHOOK_PATH > /dev/null
-
-echo Setting up Docker...
-mkdir dockercfg ; cd dockercfg
-echo -e $KEY > key.pem
-echo -e $CA_CERT > ca.pem
-echo -e $CERT > cert.pem
-cd ..
+curl -X 'POST' --silent --data-binary '{"text":"A new build for the auth service has started."}' $SLACK_WEBHOOK_PATH > /dev/null
 
 echo Building projects using gradle...
 ./gradlew build 
@@ -21,13 +14,32 @@ rc=$?
 if [ $rc != 0 ]
 then
   echo "Gradle build failed, will NOT perform Docker steps."
+  curl -X 'POST' --silent --data-binary '{"text":"Build for the auth service has failed."}' $SLACK_WEBHOOK_PATH > /dev/null
   exit 1
 fi
+
+echo Setting up Docker...
+mkdir dockercfg ; cd dockercfg
+echo -e $KEY > key.pem
+echo -e $CA_CERT > ca.pem
+echo -e $CERT > cert.pem
+
+echo $PWD
+echo $WORKSPACE/dockercfg
+cd ..
 
 echo Building and Starting Docker Image...
 cd auth-wlpcfg
 
 ../gradlew buildDockerImage 
+rc=$?
+if [ $rc != 0 ]
+then
+  echo "Gradle build failed, will NOT perform Docker steps."
+  curl -X 'POST' --silent --data-binary '{"text":"Build for the auth service has failed."}' $SLACK_WEBHOOK_PATH > /dev/null
+  exit 1
+fi
+
 ../gradlew stopCurrentContainer 
 ../gradlew removeCurrentContainer
 ../gradlew startNewEtcdContainer
