76cdf11547789f86448f808889f21b22d0374d59
Ozzy Osborne <ozzy@ca.ibm.com>
2016-08-30 16:36:05 -0400

    Stop Using Gradle Plugin
    Signed-off-by: Ozzy Osborne <ozzy@ca.ibm.com>

--- idsBuild.sh
+++ idsBuild.sh
@@ -8,42 +8,52 @@
 echo Informing Slack...
 curl -X 'POST' --silent --data-binary '{"text":"A new build for the auth service has started."}' $SLACK_WEBHOOK_PATH > /dev/null
 
-echo Building projects using gradle...
-./gradlew build 
-rc=$?
-if [ $rc != 0 ]
-then
-  echo "Gradle build failed, will NOT perform Docker steps."
-  curl -X 'POST' --silent --data-binary '{"text":"Build for the auth service has failed."}' $SLACK_WEBHOOK_PATH > /dev/null
-  exit 1
-fi
-
 echo Setting up Docker...
 mkdir dockercfg ; cd dockercfg
 echo -e $KEY > key.pem
 echo -e $CA_CERT > ca.pem
 echo -e $CERT > cert.pem
-
-echo $PWD
-echo $WORKSPACE/dockercfg
+echo Key `echo $KEY | md5sum`
+echo Ca Cert `echo $CA_CERT | md5sum`
+echo Cert `echo $CERT | md5sum`
 cd ..
 
-export DOCKER_CERT_PATH=$WORKSPACE/dockercfg
-echo Building and Starting Docker Image using $DOCKER_CERT_PATH
+echo Obtaining docker.
+wget http://security.ubuntu.com/ubuntu/pool/main/a/apparmor/libapparmor1_2.8.95~2430-0ubuntu5.3_amd64.deb -O libapparmor.deb
+sudo dpkg -i libapparmor.deb
+rm libapparmor.deb
+wget https://get.docker.com/builds/Linux/x86_64/docker-1.9.1 --quiet -O docker
+chmod +x docker
 
-cd auth-wlpcfg
-
-../gradlew --no-daemon buildDockerImage 
-rc=$?
-if [ $rc != 0 ]
+echo Building projects using gradle...
+./gradlew build
+if [ $? != 0 ]
 then
   echo "Gradle build failed, will NOT perform Docker steps."
   curl -X 'POST' --silent --data-binary '{"text":"Build for the auth service has failed."}' $SLACK_WEBHOOK_PATH > /dev/null
-  exit 1
+  exit -1
+else
+  cd auth-wlpcfg
+  ../docker build -t gameon-auth -f Dockerfile .
+  if [ $? != 0 ]
+  then
+    echo "Docker build failed, will NOT attempt to stop/rm/start-new-container."
+    curl -X 'POST' --silent --data-binary '{"text":"Docker Build for the auth service has failed."}' $SLACK_WEBHOOK_PATH > /dev/null
+    exit -2
+  else
+    echo Attempting to remove old containers.
+    ../docker stop -t 0 gameon-auth || true
+    ../docker rm gameon-auth || true
+    echo Starting new container.
+    ../docker run -d -p 9089:9080 -p 9449:9443 --link etcd -e LICENSE=accept -e ETCDCTL_ENDPOINT=http://etcd:4001 --name=gameon-auth gameon-auth
+    if [ $? != 0 ]
+    then
+      echo "Docker run failed.. it's too late.. the damage is done already."
+      curl -X 'POST' --silent --data-binary '{"text":"Docker Run for the auth service has failed."}' $SLACK_WEBHOOK_PATH > /dev/null
+      exit -3
+    else
+      cd ..
+      rm -rf dockercfg
+    fi
+  fi
 fi
-
-../gradlew --no-daemon stopCurrentContainer 
-../gradlew --no-daemon removeCurrentContainer
-../gradlew --no-daemon startNewEtcdContainer
-
-rm -rf dockercfg
