e73d0bf06d4a455c58637a058d3dc9640767bca5
Ozzy Osborne <bardweller@gmail.com>
2018-01-05 13:23:53 -0500

    Message hub jar lives in server path
    Previous cert change missed that the client jar needed to be in server path

--- auth-wlpcfg/startup.sh
+++ auth-wlpcfg/startup.sh
@@ -57,6 +57,7 @@ if [ "$ETCDCTL_ENDPOINT" != "" ]; then
   #from github, and have to use an extra config snippet to enable it.
   export MESSAGEHUB_USER=$(etcdctl get /kafka/user)
   export MESSAGEHUB_PASSWORD=$(etcdctl get /passwords/kafka)
+  cd ${SERVER_PATH}
   wget https://github.com/ibm-messaging/message-hub-samples/raw/master/java/message-hub-liberty-sample/lib-message-hub/messagehub.login-1.0.0.jar
 fi
 
